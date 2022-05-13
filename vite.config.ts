a0d0109b60af2eb4caab47b9be3ed5703bcf4f03
Zhang Yi <69628090+nus2021@users.noreply.github.com>
2022-05-13 17:10:04 +0800

    781

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
-
+//234
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh()],
